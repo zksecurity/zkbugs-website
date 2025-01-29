7bf8a7329241799fb16d6480d69ff2c9ead6ca5e
Leonidas Mavrotas <leo.mav.8@gmail.com>
2025-01-29 21:13:02 +0200

    init project

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: "localhost",
+    port: 3000,
+    open: true,
+  },
+});
