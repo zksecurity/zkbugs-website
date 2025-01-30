1359ee2269b81bb0477fd3a72a6b74321d45824b
Leonidas Mavrotas <leo.mav.8@gmail.com>
2025-01-31 00:30:36 +0200

    style footer give padding

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,27 +2,22 @@ import { styled } from "@mui/material";
 import Container from "../layout/Container";
 
 const FooterStyled = styled("footer")({
-  backgroundColor: "#111821",
-  position: "fixed",
-  bottom: 0,
-  left: 0,
+  // backgroundColor: "#111821",
   width: "100%",
-  "& .footer-wrapper": {
-    paddingTop: "4.5rem",
-    paddingBottom: "4.5rem",
-  },
+  paddingTop: "2rem",
+  paddingBottom: "2rem",
+  marginTop: "4rem",
   "& .copyright-disclaimer": {
     color: "#a3a3a3",
     backgroundColor: "##111111",
-    padding: "2.5rem 1.25rem",
   },
 });
 
 function Footer() {
   return (
-    <Container>
-      <FooterStyled></FooterStyled>
-    </Container>
+    <FooterStyled>
+      <Container></Container>
+    </FooterStyled>
   );
 }
 
