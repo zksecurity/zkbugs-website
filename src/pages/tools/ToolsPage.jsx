cde0ada4b048f376c36c1a97cf191871f935e4d8
Leonidas Mavrotas <leo.mav.8@gmail.com>
2025-02-07 01:54:27 +0200

    Add a title to security tools page

--- src/pages/tools/ToolsPage.jsx
+++ src/pages/tools/ToolsPage.jsx
@@ -1,9 +1,16 @@
+import { Typography } from "@mui/material";
 import Container from "../../components/layout/Container";
 import ToolsTable from "../../components/tools-table/ToolsTable";
 
 function ToolsPage() {
   return (
     <Container sx={{ marginTop: "4rem" }}>
+      <Typography
+        variant="h4"
+        sx={{ textAlign: "center", marginBottom: "4rem" }}
+      >
+        Security Tools
+      </Typography>
       <ToolsTable />
     </Container>
   );
